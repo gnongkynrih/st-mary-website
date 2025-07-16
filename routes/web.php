b073f7efdafdfa9ae5aedf36d766688589d749b9
Gordon Nongkynrih <gordon@dolphinze.com>
2025-07-16 17:29:56 +0530

    register

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Livewire\Login;
 use App\Livewire\ForgotPassword;
 use App\Livewire\ResetPassword;
 use App\Livewire\ChangePassword;
+use App\Livewire\Register;
 
 Route::get('/', HomePage::class);   
 
@@ -15,7 +16,7 @@ Route::get('/admin/hero-slider-management', HeroSliderManagement::class)
     ->name('admin.hero-slider-management'); 
 Route::get('/admin/service-management', ServiceManagement::class)
     ->name('admin.service-management');
-
+Route::get('/register',Register::class)->name('register');
 Route::get('/login',Login::class)->name('login');
 Route::get('forgot-password',ForgotPassword::class)->name('forgot-password');
 Route::get('reset-password',ResetPassword::class)->name('reset-password');
