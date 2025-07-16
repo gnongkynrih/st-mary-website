86e8b76559522e83cd2215f22b4ba729acbf4e15
Gordon Nongkynrih <gordon@dolphinze.com>
2025-07-16 19:22:27 +0530

    registration

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,14 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Livewire\HomePage;
-use App\Livewire\Admin\HeroSliderManagement;
-use App\Livewire\Admin\ServiceManagement;
 use App\Livewire\Login;
-use App\Livewire\ForgotPassword;
+use App\Livewire\HomePage;
+use App\Livewire\Registration;
 use App\Livewire\ResetPassword;
 use App\Livewire\ChangePassword;
-use App\Livewire\Register;
+use App\Livewire\ForgotPassword;
+use Illuminate\Support\Facades\Route;
+use App\Livewire\Admin\ServiceManagement;
+use App\Livewire\Admin\HeroSliderManagement;
 
 Route::get('/', HomePage::class);   
 
@@ -16,7 +16,8 @@ Route::get('/admin/hero-slider-management', HeroSliderManagement::class)
     ->name('admin.hero-slider-management'); 
 Route::get('/admin/service-management', ServiceManagement::class)
     ->name('admin.service-management');
-Route::get('/register',Register::class)->name('register');
+    
+Route::get('/register',Registration::class)->name('register');
 Route::get('/login',Login::class)->name('login');
 Route::get('forgot-password',ForgotPassword::class)->name('forgot-password');
 Route::get('reset-password',ResetPassword::class)->name('reset-password');
